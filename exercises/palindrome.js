160ae600a5695ce877a217b3038ae9ebba89610e
Pepe García <pepe@pepegar.com>
2020-03-09 17:21:44 +0100

    add exercises from class

--- exercises/palindrome.js
+++ exercises/palindrome.js
@@ -1,7 +1,24 @@
 "use strict";
 
-function checkPalindrome(arr) {
+function eq(a, b) {
+    if (a.length !== b.length) {
+        return false;
+    } else {
+        for (let i in a) {
+            if (a[i] !== b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
-function eq(a, b) {
+function checkPalindrome(arr) {
+
+    let copy = arr.slice().reverse();
+
+    return eq(arr, copy);
+
 }
+
+console.log(checkPalindrome([1,2,3,2,1]))
